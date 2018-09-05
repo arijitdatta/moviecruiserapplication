02ad863ca424b30863a2c29bb2766a9f8e43ee74
Ubuntu <ubuntu@ip-172-31-83-43.ec2.internal>
2018-09-05 02:06:54 +0000

    Initial Commit

---
+++ src/main/java/com/stackroute/moviecruiserserverapplication/MoviecruiserserverapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.moviecruiserserverapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecruiserserverapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecruiserserverapplicationApplication.class, args);
+	}
+}
