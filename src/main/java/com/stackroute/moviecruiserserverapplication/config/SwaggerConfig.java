49e8a95124fecde590dae2e11cc1ec78f1c4935d
Ubuntu <ubuntu@ip-172-31-83-43.ec2.internal>
2018-09-10 07:06:38 +0000

    Updated Swagger Config

--- src/main/java/com/stackroute/moviecruiserserverapplication/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/moviecruiserserverapplication/config/SwaggerConfig.java
@@ -23,9 +23,9 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 		}
 	
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources");
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
