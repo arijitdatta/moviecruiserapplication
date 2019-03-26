f9264a39a55bde20ce80e73a7666287f86b126c0
Ubuntu <ubuntu@ip-172-31-81-66.ec2.internal>
2019-03-26 10:21:41 +0000

    Backend minor check to see if CI/CD is working

--- MovieCruiserServerApplication/src/main/java/com/stackroute/moviecruiserserverapplication/restcontroller/GreetingController.java
+++ MovieCruiserServerApplication/src/main/java/com/stackroute/moviecruiserserverapplication/restcontroller/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Arijit") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "ArijitDatta") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
